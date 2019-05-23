4f6fbe1a1be33c6bafc13c29ba86c1ad264a3138
github username <github email address>
2019-05-23 09:52:04 -0700

    added skills and link seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,31 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 job1 = Job.create!(
-    company: "Google", title:"CEO", 
-    summary: "I sold the world into advertising slavery", 
-    start_month: "March", start_year: 2002, 
-    end_month: "September", end_year:2015, 
-    responsibilities:[""], 
+    company: "Google", title:"CEO",
+    summary: "I sold the world into advertising slavery",
+    start_month: "March", start_year: 2002,
+    end_month: "September", end_year:2015,
+    responsibilities:[""],
     skills_used:[])
+
+link1 = Link.create!(
+    title: "Githerb", url: "https://www.github.com",
+    summary: "It's a site what u want from me"
+)
+
+link2 = Link.create(
+    title: "The Internet", url: "https://www.google.com",
+    summary: "Advertising hell"
+)
+
+link3 = Link.create(
+    title: "Ableton", url: "https://www.ableton.com/en/",
+    summary: "A website from a company from a time in a place where the sea never ends into an infinite expanse of merpeople and crystal gazing spheres of liquid thought"
+)
+
+skill1 = Skill.create!(name: "Ruby")
+skill2 = Skill.create(name: "Rails")
+skill3 = Skill.create(name: "Javascript")
+skill4 = Skill.create(name: "React")
+skill5 = Skill.create(name: "Panic")
+skill6 = Skill.create(name: "Vomit")

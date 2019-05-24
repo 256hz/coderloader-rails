8a287609f2eb9f74c61514eb0a246af160f555e0
github username <github email address>
2019-05-24 13:50:50 -0700

    fixed img_url missing from jobs serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,3 +1,3 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :company, :title, :summary, :start_month, :start_year, :end_month, :end_year, :responsibilities, :skills_used
+  attributes :id, :company, :title, :summary, :start_month, :start_year, :end_month, :end_year, :responsibilities, :skills_used, :img_url
 end

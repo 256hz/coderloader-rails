ef743a4eb4e6ecdea1dc7d58b7f66fc5694e9868
256hz <a.d@256hz.com>
2019-05-24 14:34:32 -0700

    added bio & intro to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :img_url
+  attributes :id, :first_name, :last_name, :email, :phone, :img_url, :bio, :intro
 end

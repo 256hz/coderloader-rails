715739da0304842caad2c32618a401063840ef23
github username <github email address>
2019-05-24 13:08:49 -0700

    updated skills serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,3 +1,3 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :company, :title, :summary, :start_month, :start_year, :end_month, :end_year
+  attributes :id, :company, :title, :summary, :start_month, :start_year, :end_month, :end_year, :responsibilities, :skills_used
 end

49fd8c6172068f9de3aa489f5900dfbeb2c03055
256hz <a.d@256hz.com>
2019-05-29 10:21:00 -0700

    added has_many & belongs_to to users & models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+    has_many :githubs
+    has_many :jobs
+    has_many :skills
+    has_many :links
+    has_many :interests
+    has_many :honors
+
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
     validates :username, presence: :true

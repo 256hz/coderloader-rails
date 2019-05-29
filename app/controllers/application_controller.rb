a24522c97ba343640b551e5e769951339dc6a2b6
256hz <a.d@256hz.com>
2019-05-29 11:32:41 -0700

    auth now returns properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,26 @@ class ApplicationController < ActionController::API
  
     def encode_token(payload)
       # should store secret in env variable
+      puts "encode token:", JWT.encode(payload, 'my_s3cr3t')
       JWT.encode(payload, 'my_s3cr3t')
     end
    
     def auth_header
       # { Authorization: 'Bearer <token>' }
+      puts "request.headers:", request.headers['Authorization']
       request.headers['Authorization']
     end
    
     def decoded_token
       if auth_header
         token = auth_header.split(' ')[1]
+        puts "token from headers:", token
         # header: { 'Authorization': 'Bearer <token>' }
         begin
+          puts "decoded token:", JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
           JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
         rescue JWT::DecodeError
+          puts "decode error"
           nil
         end
       end
@@ -26,11 +31,13 @@ class ApplicationController < ActionController::API
     def current_user
       if decoded_token
         user_id = decoded_token[0]['user_id']
+        puts "found user id:", user_id
         @user = User.find_by(id: user_id)
       end
     end
    
     def logged_in?
+        puts "logged in?:", current_user
       !!current_user
     end
    

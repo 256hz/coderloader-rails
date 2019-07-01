9ae4715456202073c2729b559f3d7d5bf2896449
wardou2 <ward92@gmail.com>
2019-07-01 12:14:25 -0700

    fixed cors route

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'portfolio.256hz.com', 'localhost:3001'
+    origins 'https://douglasward-portfolio.herokuapp.com', 'localhost:3001'
 
     resource '*',
       headers: :any,

f9d4d7d37daf2741a51eb8ca58e514eb9450b0b7
Abe Dolinger <Abe Dolinger>
2019-08-05 16:05:44 -0700

    changed order of CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'portfolio.256hz.com', 'localhost:3001', 'coderloader-react.netlify.com'
+    origins 'coderloader-react.netlify.com', 'portfolio.256hz.com', 'localhost:3001' 
 
     resource '*',
       headers: :any,

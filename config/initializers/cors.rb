8937aa089527eb6a1ec7b01243955608fa7b18a2
Abe Dolinger <Abe Dolinger>
2019-08-05 16:01:21 -0700

    changed CORS for netlify frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'portfolio.256hz.com', 'localhost:3001'
+    origins 'portfolio.256hz.com', 'localhost:3001', 'coderloader-react.netlify.com'
 
     resource '*',
       headers: :any,

a854ede354db481293e85551d236c2ded3bc40da
Abe Dolinger <Abe Dolinger>
2019-08-07 19:39:09 -0700

    removed https from cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://portfolio.256hz.com', 'coderloader-react.netlify.com', 'localhost:3001' 
+    origins 'portfolio.256hz.com', 'coderloader-react.netlify.com', 'localhost:3001' 
 
     resource '*',
       headers: :any,

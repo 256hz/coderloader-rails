6b9e06132f1361ec079c6f244f37727005b557bf
Abe Dolinger <Abe Dolinger>
2019-08-07 19:34:06 -0700

    added https to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'coderloader-react.netlify.com', 'portfolio.256hz.com', 'localhost:3001' 
+    origins 'https://portfolio.256hz.com', 'coderloader-react.netlify.com', 'localhost:3001' 
 
     resource '*',
       headers: :any,
